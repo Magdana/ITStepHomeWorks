4b4d3a920ca083cffd23d3f046feadb218e08034
Magda GvirjiSvili <magdagvirjishvili@gmail.com>
2025-09-11 18:11:27 +0400

    add Discount Calculation

--- HomeWork1/Program.cs
+++ HomeWork1/Program.cs
@@ -1,4 +1,5 @@
-﻿// 1. Login Authentication
+﻿#region
+// 1. Login Authentication
 
 Console.WriteLine("Registration");
 Console.WriteLine("-------------");
@@ -30,15 +31,17 @@ else
     Console.WriteLine("Invalid credentials!");
 }
 Console.ReadKey();
+#endregion
 
 
+#region
 //2. Grade Calculator
 
 Console.WriteLine("Grade Calculator");
 Console.WriteLine("----------------");
 Console.Write("Enter your score (0-100): ");
 int input = int.Parse(Console.ReadLine().Trim());
-if(input < 0 || input > 100)
+if (input < 0 || input > 100)
 {
     Console.WriteLine("Score must be between 0 and 100.");
     Console.ReadLine();
@@ -64,3 +67,35 @@ else
     Console.WriteLine("Grade: A+");
     Console.ReadLine();
 }
+
+#endregion
+
+
+#region
+// 3.Discount Calculation
+Console.WriteLine("Discount Calculation");
+Console.WriteLine("--------------------");
+Console.Write("Enter the total purchase amount: ");
+decimal amount = decimal.Parse(Console.ReadLine().Trim());
+if (amount <= 0)
+{
+    Console.WriteLine("Amount cannot be negative or null.");
+    Console.ReadLine();
+    return;
+}
+decimal discount;
+decimal amountToPay;
+if (amount > 1000)
+{
+    discount = amount * 0.10m;
+    amountToPay = amount - discount;
+}
+else
+{
+    discount = amount * 0.05m;
+    amountToPay = amount - discount;
+}
+Console.WriteLine($"Discount: {discount}, amount to pay {amountToPay}");
+Console.ReadLine();
+
+#endregion
