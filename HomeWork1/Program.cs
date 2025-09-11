1239f60553f7dd6663bc77f9b12daeec92098303
Magda GvirjiSvili <magdagvirjishvili@gmail.com>
2025-09-11 16:41:59 +0400

    add Login Authentication

---
+++ HomeWork1/Program.cs
@@ -0,0 +1,26 @@
+﻿// 1. Login Authentication
+
+Console.WriteLine("Registration");
+Console.WriteLine("-------------");
+Console.Write("Enter your username: ");
+string username = Console.ReadLine() ?? string.Empty;
+Console.Write("Enter your password: ");
+string password = Console.ReadLine() ?? string.Empty;
+
+Console.Clear();
+
+Console.WriteLine("Login");
+Console.WriteLine("------");
+Console.Write("Enter your username: ");
+string loginUsername = Console.ReadLine() ?? string.Empty;
+Console.Write("Enter your password: ");
+string loginPassword = Console.ReadLine() ?? string.Empty;
+if (loginUsername == username && loginPassword == password)
+{
+    Console.WriteLine("Login Successful!");
+}
+else
+{
+    Console.WriteLine("Invalid credentials!");
+}
+Console.ReadKey();
