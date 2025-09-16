b490a40e88edd52571533fcabcc708f2c7d8784b
Magda GvirjiSvili <magdagvirjishvili@gmail.com>
2025-09-16 15:26:42 +0400

    add Sum of Numbers 1–100

--- ITStepHomeWorks/HomeWork2/Program.cs
+++ ITStepHomeWorks/HomeWork2/Program.cs
@@ -1,10 +1,22 @@
 ﻿#region
 
 //Print Numbers 1–10
-Console.WriteLine("numbers 1 - 10");
 for (int i = 1; i <= 10; i++)
 {
     Console.WriteLine(i);
 }
+
+#endregion
+
+#region
+
+// Sum of Numbers 1–100
+int sum = 0;
+for (int i = 1; i <= 100; i++)
+{
+    sum += i;
+}
+Console.WriteLine($"Sum = " + sum);
+
 
 #endregion
