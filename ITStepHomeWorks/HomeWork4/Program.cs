3cc9381386f904141a82e1c0a5eca19636a4c0a5
Magda GvirjiSvili <magdagvirjishvili@gmail.com>
2025-09-20 13:19:40 +0400

    add half pyramid

---
+++ ITStepHomeWorks/HomeWork4/Program.cs
@@ -0,0 +1,24 @@
+﻿#region Half Pyramid of Stars
+//დაწერეთ პროგრამა რომელიც გამოიტანს კონსოლში ფიფქებით შედგენილ ნახევარპირამიდის ფორმას.
+
+Console.Write("enter the number : ");
+int number;
+while (!int.TryParse(Console.ReadLine(), out number))
+{
+    Console.Write("Invalid input. Please enter a valid integer: ");
+}
+
+string result = "";
+
+for (int i = 1; i <= number; i++)
+{
+    for(int j = 1; j <= i; j++)
+    {
+        result += "*";
+    }
+    result += "\n";
+}
+Console.WriteLine(result);
+Console.ReadLine();
+
+#endregion
