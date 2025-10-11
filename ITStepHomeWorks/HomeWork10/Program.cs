06b21196acb1430774eaa49a1b724033a4e7b435
Magda GvirjiSvili <magdagvirjishvili@gmail.com>
2025-10-11 15:49:13 +0400

    add Task 1: Create and Implement an Interface

---
+++ ITStepHomeWorks/HomeWork10/Program.cs
@@ -0,0 +1,28 @@
+﻿//Task 1: Create and Implement an Interface
+
+List<IAnimal> animals = new List<IAnimal>();
+animals.Add(new Dog());
+animals.Add(new Cat());
+foreach (var animal in animals)
+{
+    animal.MakeSound(); 
+}
+
+public interface IAnimal
+{
+    void MakeSound();
+}  
+public class Dog : IAnimal
+{
+    public void MakeSound()
+    {
+        Console.WriteLine("Woof!");
+    }
+}
+public class Cat : IAnimal
+{
+    public void MakeSound()
+    {
+        Console.WriteLine("Meow!");
+    }
+}
